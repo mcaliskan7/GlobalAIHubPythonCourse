ffd738a17539e92b20a06c2d11cc37ded73216c0
mcaliskan7 <59294472+mcaliskan7@users.noreply.github.com>
2020-12-22 00:00:56 +0300

    Add files via upload

---
+++ hw1_printing_types.py
@@ -0,0 +1,12 @@
+n0 = input("Enter the 1st value: ")
+n1 = input("Enter the 2nd value: ")
+n2 = input("Enter the 3rd value: ")
+n3 = input("Enter the 4th value: ")
+n4 = input("Enter the 5th value: ")
+
+print(n0,n1,n2,n3,n4)
+print("Type of {}: {}".format(n0,type(n0)))
+print("Type of {}: {}".format(n1,type(n1)))
+print("Type of {}: {}".format(n2,type(n2)))
+print("Type of {}: {}".format(n3,type(n3)))
+print("Type of {}: {}".format(n4,type(n4)))
