d560d6ad910e0fddbd94667157b6e634bd67eda6
mcaliskan7 <59294472+mcaliskan7@users.noreply.github.com>
2020-12-22 19:53:56 +0300

    Update hw1_printing_types.py

--- hw1_printing_types.py
+++ hw1_printing_types.py
@@ -1,10 +1,11 @@
-n0 = input("Enter the 1st value: ")
-n1 = input("Enter the 2nd value: ")
-n2 = input("Enter the 3rd value: ")
-n3 = input("Enter the 4th value: ")
-n4 = input("Enter the 5th value: ")
+n0 = int(input("Enter an integer: ")) #allows only integers as integer type
+n1 = float(input("Enter a float: "))  #allows integers and floats as float type
+n2 = input("Enter a string: ")        #allows anything as string type 
+n3 = float(input("Enter a float: "))  #allows integers and floats as float type
+n4 = int(input("Enter an integer: ")) #allows only integers as integer type
 
 print(n0,n1,n2,n3,n4)
+
 print("Type of {}: {}".format(n0,type(n0)))
 print("Type of {}: {}".format(n1,type(n1)))
 print("Type of {}: {}".format(n2,type(n2)))
